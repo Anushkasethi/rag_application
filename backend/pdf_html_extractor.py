52cc97416339bfaa1a318db38f90c8910f75b19d
Anushka <anushka.sethi1809@gmail.com>
2025-03-17 20:28:10 +0530

    using pdfplumber

--- backend/pdf_html_extractor.py
+++ backend/pdf_html_extractor.py
@@ -1,13 +1,20 @@
-import fitz  # PyMuPDF
+# import fitz  # PyMuPDF
 from bs4 import BeautifulSoup
 
-def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    return text
+# def extract_text_from_pdf(pdf_path):
+#     doc = fitz.open(pdf_path)
+#     text = ""
+#     for page in doc:
+#         text += page.get_text()
+#     return text
+import pdfplumber
 
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
 def extract_text_from_html(html_path):
     with open(html_path, "r", encoding="utf-8") as file:
         soup = BeautifulSoup(file, "html.parser")
